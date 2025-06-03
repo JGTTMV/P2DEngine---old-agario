c53746106b610c9d72e6e556ff6b0d57d8df289f
JUANJJJV <jotavaldebenitop@gmail.com>
2025-06-03 12:03:22 -0400

    the whole thing
    yea yea

---
+++ P2DEngine/GameObjects/myPhysicsBlock.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using P2DEngine.GameObjects;
+using P2DEngine.GameObjects.Collisions;
+
+namespace P2DEngine
+{
+    public class myPhysicsBlock : myPhysicsGameObject
+    {
+        public myPhysicsBlock(float x, float y, float sizeX, float sizeY, Color color) : base(x, y, sizeX, sizeY, color)
+        {
+        }
+
+        public myPhysicsBlock(float x, float y, float sizeX, float sizeY, Image image) : base(x, y, sizeX, sizeY, image)
+        {
+        }
+
+        // Crear el collider de caja. Esto se llamara automatico.
+        public override void CreateCollider(float sizeX, float sizeY)
+        {
+            collider = new BoxCollider2D(sizeX, sizeY, this);
+        }
+
+        // Se dibuja igual que un game object.
+        public override void Draw(Graphics g, Vector position, Vector size)
+        {
+            if(image == null)
+            {
+                g.FillRectangle(brush, (float)position.X, (float)position.Y, (float)size.X, (float)size.Y);
+            }
+            else
+            {
+                g.DrawImage(image, (float)position.X, (float)position.Y, (float)size.X, (float)size.Y);
+            }
+        }
+
+
+        // Me dio flojera cambiarlo para que el nombre se quedase en Update, pero este se supone que es el Update que conocen
+        // de todo el semestre :)
+        public override void UpdateGameObject(float deltaTime)
+        {
+        }
+    }
+
+    public class Medkit : myPhysicsBlock
+    {
+        public Medkit(float x, float y, float sizeX, float sizeY)
+            : base(x, y, sizeX, sizeY, Color.Green)
+        {
+        }
+
+        public override void Draw(Graphics g, Vector position, Vector size)
+        {
+            if (image != null)
+            {
+                g.DrawImage(image, (float)position.X, (float)position.Y, (float)size.X, (float)size.Y);
+            }
+            else
+            {
+                g.FillRectangle(Brushes.Green, (float)position.X, (float)position.Y, (float)size.X, (float)size.Y);
+            }
+        }
+    }
+
+    public class WallBlock : myPhysicsBlock
+    {
+        public WallBlock(float x, float y, float sizeX, float sizeY)
+            : base(x, y, sizeX, sizeY, Color.Black)
+        {
+        }
+
+        public override void Draw(Graphics g, Vector position, Vector size)
+        {
+            g.FillRectangle(Brushes.DarkBlue, (float)position.X, (float)position.Y, (float)size.X, (float)size.Y);
+            using (var pen = new Pen(Color.Black, 2))
+            {
+                g.DrawRectangle(pen, (float)position.X, (float)position.Y, (float)size.X, (float)size.Y);
+            }
+        }
+    }
+}
---
+++ P2DEngine/GameObjects/myPlayer.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Drawing;
+using System.Windows;
+using System.Windows.Forms;
+using P2DEngine.Managers;
+
+namespace P2DEngine.GameObjects
+{
+    public class myPlayer : myPhysicsBlock
+    {
+        private Image[] sprites = new Image[4]; //0: front, 1: back, 2: left, 3: right
+        Image bulletSprite;
+        private int currentDirection = 0; //0: front, 1: back, 2: left, 3: right
+        private float speed = 200f; //Velocidad de movimiento
+        private int vida = 100; //Vida del jugador
+        private float invulnerableTime = 1.0f; //1 segundo de invulnerabilidad inicial
+
+        public int Vida { get { return vida; } }
+        public bool IsInvulnerable { get { return invulnerableTime > 0; } }
+
+        public myPlayer(float x, float y, float sizeX, float sizeY)
+            : base(x, y, sizeX, sizeY, Color.Transparent)
+        {
+            //Carga los sprites
+            sprites[0] = myImageManager.Get("gas-front");
+            sprites[1] = myImageManager.Get("gas-back");
+            sprites[2] = myImageManager.Get("gas-left");
+            sprites[3] = myImageManager.Get("gas-right");
+
+            affectedByGravity = false;
+        }
+
+        public override void Draw(Graphics g, Vector position, Vector size)
+        {
+            //Dibuja el sprite correspondiente a la direccion actual
+            if (sprites[currentDirection] != null)
+            {
+                g.DrawImage(sprites[currentDirection], (float)position.X, (float)position.Y, (float)size.X, (float)size.Y);
+            }
+            else
+            {
+                base.Draw(g, position, size); //Fallback si no hay sprite
+            }
+        }
+
+        public override void UpdateGameObject(float deltaTime)
+        {
+            if (invulnerableTime > 0) //Genera frames de invulnerabilidad
+                invulnerableTime -= deltaTime;
+        }
+
+        public void SetLife() { vida = 100; }
+
+        public void UpdateDirection(float deltaTime)
+        {
+            //Solo permite el movimiento para un eje a la vez (no diagonal)
+            bool up = myInputManager.IsKeyPressed(Keys.W);
+            bool down = myInputManager.IsKeyPressed(Keys.S);
+            bool left = myInputManager.IsKeyPressed(Keys.A);
+            bool right = myInputManager.IsKeyPressed(Keys.D);
+
+            //Prioridad: vertical sobre horizontal
+            if (up && !down && !left && !right)
+            {
+                y -= speed * deltaTime;
+                currentDirection = 1; // back
+            }
+            else if (down && !up && !left && !right)
+            {
+                y += speed * deltaTime;
+                currentDirection = 0; // front
+            }
+            else if (left && !up && !down && !right)
+            {
+                x -= speed * deltaTime;
+                currentDirection = 2; // left
+            }
+            else if (right && !up && !down && !left)
+            {
+                x += speed * deltaTime;
+                currentDirection = 3; // right
+            }
+        }
+
+        //Metodo para mantener al jugador dentro de los limites de la pantalla
+        public void KeepInBounds(float screenWidth, float screenHeight)
+        {
+            if (x < 0) x = 0;
+            if (x + sizeX > screenWidth) x = screenWidth - sizeX;
+            if (y < 0) y = 0;
+            if (y + sizeY > screenHeight) y = screenHeight - sizeY;
+        }
+
+        public void PerderVida(int cantidad)
+        {
+            if (IsInvulnerable && cantidad > 0) return;
+            vida -= cantidad;
+            invulnerableTime = 1.0f;
+            if (vida < 0) vida = 0;
+        }
+
+        public myBullet Shoot()
+        {
+            //Dispara en la direccion actual usando siempre la textura de bullet
+            bulletSprite = myImageManager.Get("bullet");
+            if (currentDirection == 0)
+            {
+                return new myBullet(x + sizeX / 2 - 2, y + sizeY, 5, Color.Yellow, bulletSprite, 300, 0); // Abajo
+            }
+            else if (currentDirection == 1)
+            {
+                return new myBullet(x + sizeX / 2 - 2, y - 10, 5, Color.Yellow, bulletSprite, -300, 0); // Arriba
+            }
+            else if (currentDirection == 2)
+            {
+                return new myBullet(x - 10, y + sizeY / 2 - 2, 5, Color.Yellow, bulletSprite, 0, -300); // Izquierda
+            }
+            else
+            {
+                return new myBullet(x + sizeX + 2, y + sizeY / 2 - 2, 5, Color.Yellow, bulletSprite, 0, 300); // Derecha
+            }
+        }
+    }
+}
